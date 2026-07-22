96c0afccf6480c66e2bb2c653040569d2e5e368a
blaklabz <tobywatkins1@gmail.com>
2026-07-22 17:02:11 -0400

    updates

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,35 +1,60 @@
 #!/bin/bash
+
 set -e
+
+echo "Starting LaForge Dev tool installation..."
 
 ARCH=$(uname -m)
 
-echo "Installing Terragrunt for $ARCH"
+case "$ARCH" in
+    x86_64)
+        PLATFORM="amd64"
+        ;;
+    aarch64|arm64)
+        PLATFORM="arm64"
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+esac
 
-if [ "$ARCH" = "aarch64" ]; then
-  TG_ARCH="arm64"
-elif [ "$ARCH" = "x86_64" ]; then
-  TG_ARCH="amd64"
-else
-  echo "Unsupported architecture: $ARCH"
-  exit 1
-fi
+echo "Detected architecture: $ARCH ($PLATFORM)"
+
+#
+# Terragrunt
+#
+echo "Installing Terragrunt..."
 
 curl -L \
-  "https://github.com/gruntwork-io/terragrunt/releases/latest/download/terragrunt_linux_${TG_ARCH}" \
-  -o /usr/local/bin/terragrunt
+  "https://github.com/gruntwork-io/terragrunt/releases/latest/download/terragrunt_linux_${PLATFORM}" \
+  -o /tmp/terragrunt
 
-chmod +x /usr/local/bin/terragrunt
+sudo mv /tmp/terragrunt /usr/local/bin/terragrunt
+sudo chmod +x /usr/local/bin/terragrunt
 
-echo "Installing k9s"
+echo "Terragrunt installed:"
+terragrunt --version
+
+
+#
+# k9s
+#
+echo "Installing k9s..."
 
 curl -L \
-  "https://github.com/derailed/k9s/releases/latest/download/k9s_Linux_${TG_ARCH}.tar.gz" \
+  "https://github.com/derailed/k9s/releases/latest/download/k9s_Linux_${PLATFORM}.tar.gz" \
   -o /tmp/k9s.tar.gz
 
-tar -xzf /tmp/k9s.tar.gz -C /tmp
+mkdir -p /tmp/k9s
 
-mv /tmp/k9s /usr/local/bin/k9s
+tar -xzf /tmp/k9s.tar.gz -C /tmp/k9s
 
-chmod +x /usr/local/bin/k9s
+sudo mv /tmp/k9s/k9s /usr/local/bin/k9s
+sudo chmod +x /usr/local/bin/k9s
 
-echo "Done"
+echo "k9s installed:"
+k9s version --short
+
+
+echo "LaForge Dev tools installation complete!"
